f7a021f73def56ad22a34fad2a594e0016020d12
hdh6683 <hdh6683@naver.com>
2021-09-08 14:10:40 +0900

    210908_1410

---
+++ Array_Ex/Array_Ex/Array_Ex1.c
@@ -0,0 +1,9 @@
+﻿//#include <stdio.h>
+//void main(void) {
+//	int a[4] = { 1,2,3,4 };
+//	int i;
+//	for (i = 0; i < 4; i++)
+//		printf("주소 = %x  a[%d] = %d\n", (a + i), i, *(a + i));
+//	printf("a = %x  a = %x\n", a, &a);
+//	printf("a + 1 = %x  &a + 1 = %x\n", a + 1, &a + 1);
+//}
