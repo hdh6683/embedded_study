779f06ad9e73428ebb788f42839b10c7a328b6c9
hdh6683 <hdh6683@naver.com>
2021-09-08 13:02:28 +0900

    0908 오전

---
+++ Volatile_Ex/Volatile_Ex/Volatile_Ex.c
@@ -0,0 +1,13 @@
+﻿#define PA (*(volatile unsigned char*)0x30000000)
+
+void main(void) {
+	int i;
+	volatile int j;
+	init();
+	while (1) {
+		for (i = 0; i < 8; i++) {
+			display(i);
+			for (j = 0; j < 100000; j++);
+		}
+	}
+}
